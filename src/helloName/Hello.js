7436a76dc8170ddcdc74ba5a7c30d4949e9d06f3
Thanh Nguyen <ngtrungthanh2010@gmail.com>
2021-12-14 16:55:21 +0700

    update

--- src/helloName/Hello.js
+++ src/helloName/Hello.js
@@ -1,15 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import './Hello.css';
 
+
+const initState = {
+  loading: false,
+  data: [],
+  error: null
+}
+
+
+const reducer = (state, action) => {
+  switch (action) {
+    case "TANG":
+      return state + 1
+    default:
+      return state
+  }
+}
+
+const reducer2 = (state, action) => {
+  switch (action.type) {
+    case "GAN_GIA_TRI":
+      return action.data;
+    default:
+      return state
+  }
+}
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USER_REQUEST":
+      return {
+        ...state,
+        loading: true
+      }
+    case "GET_USER_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        data: action.data
+      }
+    case "GET_USER_ERROR":
+      return {
+        ...state,
+        data: [],
+        error: action.data
+      }
+    default:
+      return state
+  }
+}
 const Hello = () => {
-  const [valueInput, setState] = useState('thanh')
+  const [valueInput, setState] = useState('thanh');
+  const [count, dispatch] = useReducer(reducer, 0);
+  const [count2, dispatch2] = useReducer(reducer2, 0);
+  const [user, userDispatch] = useReducer(userReducer, initState);
+
+  const getUsers = () => {
+    userDispatch({
+      type: 'GET_USER_REQUEST'
+    });
+
+    setTimeout(() => {
+      fetch('https://reqres.in/api/users')
+        .then(res => res.json())
+        .then(res => {
+          userDispatch({
+            type: "GET_USER_SUCCESS",
+            data: res
+          });
+        })
+        .catch(err => {
+          userDispatch({
+            type: "GET_USER_ERROR",
+            data: err
+          });
+        })
+
+    }, 2000)
+
+  }
 
   return (
     <div>
+      <button onClick={getUsers}> get user</button>
+      {user.loading ? <p>loading...</p> : <p>{JSON.stringify(user)}</p>}
+
+      <button onClick={() => dispatch('TANG')}>{count}Tang</button>
+
+      <button onClick={() =>
+        dispatch2({
+          type: 'GAN_GIA_TRI',
+          data: 10
+        })
+      }> {count2} Gan gia tri</button>
+
       <input value={valueInput} onChange={e => { setState(e.target.value) }} />
       <p>
         Hello {valueInput}!
-        </p>
+      </p>
     </div>
   )
 }
